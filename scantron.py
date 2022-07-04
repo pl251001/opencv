1de94bbccf1445b35573ac81092ce420bb4ad077
Lucas Choi <90462365+pl251001@users.noreply.github.com>
2022-07-03 21:56:36 -0400

    Update scantron.py

--- scantron.py
+++ scantron.py
@@ -1,3 +1,20 @@
 Import cv2
+Import random
 
-num
+num = int(input("number of scantrons: "))
+img = cv2.imread()
+pt = []
+horiDif = 
+vertDif = 
+
+for x in range(num):
+    scan = img.copy()
+    options = [0, 1, 2, 3, 4] 
+    answers = random.choices(options, weights=(225, 225, 225, 225, 100), k=30) 
+    count = 0
+    for y in answers:
+        choice = [pt[0] + (horiDif*y), pt[1] + (vertDif*count)]
+        count = count + 1
+        cv2.circle(scan, choice, radius=10, (0,0,0), thickness = 5)
+    cv2.imshow(f"scantron {x+1}", scan)
+    cv2.waitKey(5)
